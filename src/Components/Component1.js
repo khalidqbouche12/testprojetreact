dfb6f2e79e60936105bf61fd48daf97de5b7555e
khalidqbouche <khalidqbouche12@gmail.com>
2020-07-23 20:10:34 +0100

    add first component 1 in our project

---
+++ src/Components/Component1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Component1 extends React.Component{
+    render(){
+        return (
+            <div>
+                hello maroc
+            </div>
+        );
+    }
+}
+export default Component1;
