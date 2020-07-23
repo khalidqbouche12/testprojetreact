0d177f9da8b04ac497a75e336cd396d0de4144c5
khalidqbouche <khalidqbouche12@gmail.com>
2020-07-23 20:50:32 +0100

    add component 2

---
+++ src/components/Component2.js
@@ -0,0 +1,11 @@
+import React from 'react';
+class Component2 extends React.Component{
+    render(){
+        return (
+            <div>
+                heelo
+            </div>
+        );
+    }
+}
+export default Component2;
